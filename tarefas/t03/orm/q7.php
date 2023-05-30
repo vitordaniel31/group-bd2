dd2a42e9c8c6ccf1bd86934fd1951d51c64ba92f
ketwy <ketllyazevedo14@gmail.com>
2023-05-30 10:47:00 -0300

    fix: criando e alterando os indices #12

--- tarefas/t03/orm/q7.php
+++ tarefas/t03/orm/q7.php
@@ -4,7 +4,9 @@ require 'vendor/autoload.php';
 
 $capsule = require 'app/config/connection.php';
 
-$capsule::connection()->getPdo()->exec('ALTER TABLE projeto ADD INDEX idx_codigo (codigo)');
-
-
-
+$capsule::connection()->getPdo()->exec('CREATE INDEX idx_funcionario_nome ON funcionario (nome)');
+$capsule::connection()->getPdo()->exec('CREATE INDEX idx_equipe_codigo ON equipe (codigo)');
+$capsule::connection()->getPdo()->exec('CREATE INDEX idx_projeto_equipe ON projeto (equipe)');
+$capsule::connection()->getPdo()->exec('CREATE INDEX idx_atividade_codAtividade ON atividade_projeto (codAtividade)');
+$capsule::connection()->getPdo()->exec('CREATE INDEX idx_atividade_codProjeto ON atividade_projeto (codProjeto)');
+$capsule::connection()->getPdo()->exec('CREATE INDEX idx_situacao_dataFim ON atividade (situacao, dataFim)');
