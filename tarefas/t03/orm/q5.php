afc8b8dd5083d5a2e53c9ed3e7a603b2255bd212
José Marques <jose.marques15@hotmail.com>
2023-05-29 23:27:09 -0300

    procedure da questão 5 #10

--- tarefas/t03/orm/q5.php
+++ tarefas/t03/orm/q5.php
@@ -1,54 +1,47 @@
 <?php
 
-include('app/config/connection.php');
-use App\Models\Projeto;
+require 'vendor/autoload.php';
 
-$projetos = Projeto::select('projeto.codigo', 'projeto.descricao', 'funcionario.nome')
-    ->selectRaw('(SELECT COUNT(*) FROM funcionario 
-        JOIN membro ON membro.codFuncionario = funcionario.codigo
-        JOIN equipe ON equipe.codigo = membro.codEquipe
-        WHERE equipe.codigo = projeto.equipe) as membroEquipeProj')
-    ->selectRaw('(SELECT DATEDIFF(CURDATE(), projeto.dataFim)
-        WHERE projeto.situacao = "Em andamento" OR projeto.situacao = "Planejado") as diasProjetoAtrasado')
-    ->selectRaw('(SELECT COUNT(*) from atividade_projeto 
-        JOIN atividade ON atividade_projeto.codAtividade = atividade.codigo
-        WHERE atividade_projeto.codProjeto = projeto.codigo) as atvProjeto')
-    ->selectRaw('(SELECT COUNT(*) from atividade
-        JOIN atividade_projeto ON atividade.codigo = atividade_projeto.codAtividade
-        WHERE atividade_projeto.codProjeto = projeto.codigo
-        AND (atividade.situacao = "Planejado" OR atividade.situacao = "Em andamento") 
-        AND atividade.dataFim < CURDATE()
-        GROUP BY projeto.codigo) as atvAtrasadasProjeto')
-    ->selectRaw('(SELECT SUM(DATEDIFF(CURDATE(), atividade.dataFim)) from atividade
-        JOIN atividade_projeto ON atividade.codigo = atividade_projeto.codAtividade
-        WHERE atividade_projeto.codProjeto = projeto.codigo
-        AND (atividade.situacao = "Planejado" OR atividade.situacao = "Em andamento") 
-        AND atividade.dataFim < CURDATE()
-        LIMIT 1) as diasAtvAtrasadas')
-    ->join('funcionario', 'funcionario.codigo', '=', 'projeto.responsavel')
-    ->join('departamento', 'departamento.codigo', '=', 'projeto.depto')
-    ->get();
+$capsule = require 'app/config/connection.php';
 
-foreach ($projetos as $projeto) {
-    echo $projeto->codigo . ' - ' . $projeto->descricao . ' - ' . $projeto->nome . "\n";
-    echo '- Total de Membros da Equipe do Projeto: ' . $projeto->membroEquipeProj . "\n";
-    if ($projeto->diasProjetoAtrasado == null) {
-        echo '- Total de Dias de Atraso do Projeto: ' . 0 . "\n";
-    }else{
-        echo '- Total de Dias de Atraso do Projeto: ' . $projeto->diasProjetoAtrasado . "\n";
-    }
-    echo '- Total de Atividades do Projeto: ' . $projeto->atvProjeto . "\n";
-    if ($projeto->atvAtrasadasProjeto == null) {
-        echo '- Total de Atividades Atrasadas do Projeto: ' . 0 . "\n";
-    }else{
-        echo '- Total de Atividades Atrasadas do Projeto: ' . $projeto->atvAtrasadasProjeto . "\n";;
-    }
-    if ($projeto->diasAtvAtrasadas == null) {
-        echo '- Total de Dias de Atividades Atrasadas: ' . 0 . "\n";
-    }else{
-        echo '- Total de Dias de Atividades Atrasadas: ' . $projeto->diasAtvAtrasadas . "\n";
-    }
+$capsule::connection()->getPdo()->exec('
+    CREATE OR REPLACE PROCEDURE relatorioAnalitico()
+    BEGIN
+        SELECT p.codigo, p.descricao, f.nome, 
+        (SELECT COUNT(*) FROM funcionario 
+            JOIN membro ON membro.codFuncionario = funcionario.codigo
+            JOIN equipe ON equipe.codigo = membro.codEquipe
+            WHERE equipe.codigo = p.equipe) as membroEquipeProj,
+        (SELECT DATEDIFF(CURDATE(), p.dataFim)
+            WHERE p.situacao = "Em andamento" OR p.situacao = "Planejado") as diasProjetoAtrasado,
+        (SELECT COUNT(*) FROM atividade_projeto
+            JOIN atividade ON atividade_projeto.codAtividade = atividade.codigo
+            WHERE atividade_projeto.codProjeto = p.codigo) as atvProjeto,
+        (SELECT COUNT(*) FROM atividade
+            JOIN atividade_projeto ON atividade.codigo = atividade_projeto.codAtividade
+            WHERE atividade_projeto.codProjeto = p.codigo
+            AND (atividade.situacao = "Planejado" OR atividade.situacao = "Em andamento")
+            AND atividade.dataFim < CURDATE()
+            GROUP BY p.codigo) as atvAtrasadasProjeto,
+        (SELECT SUM(DATEDIFF(CURDATE(), atividade.dataFim)) FROM atividade
+            JOIN atividade_projeto ON atividade.codigo = atividade_projeto.codAtividade
+            WHERE atividade_projeto.codProjeto = p.codigo
+            AND (atividade.situacao = "Planejado" OR atividade.situacao = "Em andamento")
+            AND atividade.dataFim < CURDATE()
+            LIMIT 1) as diasAtvAtrasadas
+        FROM projeto p
+        JOIN funcionario f ON f.codigo = p.responsavel
+        JOIN departamento d ON d.codigo = p.depto;
+    END');
+
+$results = $capsule::connection()->select('CALL relatorioAnalitico()');
+
+foreach ($results as $result) {
+    echo $result->codigo . ' - ' . $result->descricao . ' - ' . $result->nome . "\n";
+    echo '- Total de Membros da Equipe do Projeto: ' . $result->membroEquipeProj . "\n";
+    echo '- Total de Dias de Atraso do Projeto: ' . ($result->diasProjetoAtrasado ?? 0) . "\n";
+    echo '- Total de Atividades do Projeto: ' . $result->atvProjeto . "\n";
+    echo '- Total de Atividades Atrasadas do Projeto: ' . ($result->atvAtrasadasProjeto ?? 0) . "\n";
+    echo '- Total de Dias de Atividades Atrasadas: ' . ($result->diasAtvAtrasadas ?? 0) . "\n";
     echo "<br>";
 }
-
-?>
